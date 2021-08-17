5ba37bde7efe1b8b716af16645d3af8a3dfb0278
ohrok <oh.hymh@gmail.com>
2021-08-17 16:09:43 +0900

    bind text field to slider

--- RGBSliders/ViewController.swift
+++ RGBSliders/ViewController.swift
@@ -11,19 +11,61 @@ import RxCocoa
 
 class ViewController: UIViewController {
   
+  @IBOutlet weak var colorView: UIView!
+  @IBOutlet weak var hexLabel: UILabel!
   @IBOutlet weak var redTextField: UITextField!
+  @IBOutlet weak var greenTextField: UITextField!
+  @IBOutlet weak var blueTextField: UITextField!
+  @IBOutlet weak var redSlider: UISlider!
+  @IBOutlet weak var greenSlider: UISlider!
+  @IBOutlet weak var blueSlider: UISlider!
   
   private let disposeBag = DisposeBag()
   private let viewModel = ViewModel()
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
-    redTextField.rx.text.orEmpty
-      .compactMap { text in
-        Int(text)
-      }
+    
+    let redNum = redTextField.rx.text.orEmpty
+      .compactMap { Int($0) }
+      .filter { 0 ..< 256 ~= $0 }
+      .share()
+    
+    let greenNum = greenTextField.rx.text.orEmpty
+      .compactMap { Int($0) }
+      .filter { 0 ..< 256 ~= $0 }
+      .share()
+    
+    let blueNum = blueTextField.rx.text.orEmpty
+      .compactMap { Int($0) }
+      .filter { 0 ..< 256 ~= $0 }
+      .share()
+  
+    redNum
       .bind(to: viewModel.red)
+      .disposed(by: disposeBag)
+    
+    greenNum
+      .bind(to: viewModel.green)
+      .disposed(by: disposeBag)
+    
+    blueNum
+      .bind(to: viewModel.red)
+      .disposed(by: disposeBag)
+    
+    redNum
+      .map { Float($0) }
+      .bind(to: redSlider.rx.value)
+      .disposed(by: disposeBag)
+    
+    greenNum
+      .map { Float($0) }
+      .bind(to: greenSlider.rx.value)
+      .disposed(by: disposeBag)
+    
+    blueNum
+      .map { Float($0) }
+      .bind(to: blueSlider.rx.value)
       .disposed(by: disposeBag)
     
     viewModel.color.asObservable()
@@ -38,7 +80,5 @@ class ViewController: UIViewController {
       })
       .disposed(by: disposeBag)
   }
-  
-  
 }
 
